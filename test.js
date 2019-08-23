b803541566f92db2c2e3c688c6894be2842d21ca
YaseminLi <yaseminli@163.com>
2019-08-23 12:58:56 +0800

    构造函数原型

--- test.js
+++ test.js
@@ -1,1 +1,34 @@
-console.log(10+'1');
+let rs = {
+    h: ["id", "name"],
+    d: [[100, "Tom"], [101, "Jane"], [102, "Tom"]]
+};
+let hash = rs2MultiHash(rs, "id");
+
+function rs2MultiHash(obj, key) {
+    let hash={}
+    let arr = []
+    let keys=rs.h
+    rs.d.forEach(item => {
+        let itemObj = {}
+        itemObj[keys[0]]=item[0]
+        itemObj[keys[1]]=item[1]
+        arr.push(itemObj)
+    })
+    arr.forEach(item=>{
+        let hashKey=item[key]
+        if(!hash[hashKey]){
+            hash[hashKey]=[item]
+        }else{
+            hash[hashKey].push(item)
+        }
+    })
+    return hash
+}
+
+console.log(hash);
+
+// 结果为
+// hash = {
+// 	"Tom": [{id: 100, name: "Tom"}, {id: 102, name: "Tom"}],
+// 	"Jane": [{id: 101, name: "Jane"}]
+// };
