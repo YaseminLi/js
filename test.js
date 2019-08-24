42c246039e418680f411bf785c110864cffec178
YaseminLi <yaseminli@163.com>
2019-08-24 14:10:55 +0800

    作用域和闭包

--- test.js
+++ test.js
@@ -1,34 +1,17 @@
-let rs = {
-    h: ["id", "name"],
-    d: [[100, "Tom"], [101, "Jane"], [102, "Tom"]]
-};
-let hash = rs2MultiHash(rs, "id");
-
-function rs2MultiHash(obj, key) {
-    let hash={}
-    let arr = []
-    let keys=rs.h
-    rs.d.forEach(item => {
-        let itemObj = {}
-        itemObj[keys[0]]=item[0]
-        itemObj[keys[1]]=item[1]
-        arr.push(itemObj)
-    })
-    arr.forEach(item=>{
-        let hashKey=item[key]
-        if(!hash[hashKey]){
-            hash[hashKey]=[item]
-        }else{
-            hash[hashKey].push(item)
+function isFirstLoad() {
+    let idList = []
+    return function (id) {
+        if (idList.indexOf(id) > -1) {
+            return false
+        } else {
+            idList.push(id)
+            return true
         }
-    })
-    return hash
+    }
 }
 
-console.log(hash);
-
-// 结果为
-// hash = {
-// 	"Tom": [{id: 100, name: "Tom"}, {id: 102, name: "Tom"}],
-// 	"Jane": [{id: 101, name: "Jane"}]
-// };
+let firstLoad=isFirstLoad()
+console.log(firstLoad(10));//true
+console.log(firstLoad(10));//false
+console.log(firstLoad(20));//true
+console.log(firstLoad(20));//false
